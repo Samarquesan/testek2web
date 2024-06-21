13717b1d52e616db485d15065c179d38a03792d2
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-06-21 01:22:57 -0300

    ok

--- script.php
+++ script.php
@@ -13,7 +13,7 @@ $formato = strtolower(pathinfo($pastaArquivo, PATHINFO_EXTENSION));
 
 
 $confirmacao = mime_content_type($_FILES["arquivo"]["tmp_name"]);
-if (strpos($confirmacao, "audio/") === 0 || strpos($confirmacao, "video/") === 0 || strpos($confirmacao, "image/") === 0) {
+if (strpos($confirmacao, "audio/") === 0 || strpos($confirmacao, "video/") === 0) {
     $mensagemErro .= "Formato de arquivo não suportado! ";
     $confirmadoEnvio = 0;
 }
@@ -31,7 +31,7 @@ if ($_FILES["arquivo"]["size"] > 50000000) {
 }
 
 
-$formatoAceito = ["mp3", "wav", "wma", "mp4", "wmv", "mpg", "mov", "jpg", "jpeg", "png", "gif"];
+$formatoAceito = ["mp3", "wav", "wma", "mp4", "wmv", "mpg", "mov"];
 if (!in_array($formato, $formatoAceito)) {
     $mensagemErro .= "O formato do arquivo não é compatível! ";
     $confirmadoEnvio = 0;
@@ -43,14 +43,20 @@ if ($confirmadoEnvio == 0) {
     echo $mensagemErro;
 } else {
     if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $pastaArquivo)) {
+        if (!file_exists('arquivos.json')) {
+            file_put_contents('arquivos.json', json_encode([]));
+        }
         $envios = json_decode(file_get_contents('arquivos.json'), true) ?: [];
+
         $novoEnvio = [
             'nomeDocumento' => basename($_FILES["arquivo"]["name"]),
             'description' => $_POST['datalhesArquivo'],
             'type' => $confirmacao
         ];
         $envios[] = $novoEnvio;
+
         file_put_contents('arquivos.json', json_encode($envios));
+
         echo "O arquivo " . htmlspecialchars(basename($_FILES["arquivo"]["name"])) . " foi enviado com sucesso!";
         header("Location: index.html");
         exit(); 
